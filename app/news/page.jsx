f7939d9116bb964981cdf9ff1e05466631b17ebb
hoamx <hoamx.work@gmail.com>
2024-06-23 16:51:28 +0700

    feat: handle parallel routes

--- app/news/page.jsx
+++ app/news/page.jsx
@@ -1,24 +1,11 @@
-import Link from "next/link";
 import { DUMMY_NEWS } from "@/dummy-news";
-import Image from "next/image";
+import NewsList from "@/components/news-list";
 
 const NewsPage = () => {
   return (
     <>
       <h1>Success is the sum of small efforts, repeated day-in and day-out.</h1>
-      <ul className="news-list">
-        {DUMMY_NEWS.map((newsItem) => (
-          <li key={newsItem.id}>
-            <Link href={`/news/${newsItem.slug}`}>
-              <img
-                src={`/images/news/${newsItem.image}`}
-                alt={newsItem.title}
-              />
-              <span>{newsItem.title}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <NewsList news={DUMMY_NEWS} />
     </>
   );
 };
